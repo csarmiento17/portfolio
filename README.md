6ffaa29d27154a8af6d5b09c477f674693e519cd
Carlopher Jay Sarmiento <sarmientocarlopher@gmail.com>
2021-06-25 03:37:01 -0400

    Added hamburger menu and javascript file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Portfolio
+
+Test content here
+
+test
+
+test
+test test
+![Alt text](./assets/images/liland.jpg "Optional Title")
---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector(".hamburger-toggle");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("hamburger-open");
+});
