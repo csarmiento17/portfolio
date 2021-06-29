2e8988a5b7825d529d20ae2826135e23f3e7dcc6
Carlopher Jay Sarmiento <sarmientocarlopher@gmail.com>
2021-06-29 02:40:04 -0400

    Added js code to close hamburger menu when a selection has been clicked on the sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ const navToggle = document.querySelector(".hamburger-toggle");
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("hamburger-open");
 });
+
+const sidebarToggle = document.querySelector(".sidebar");
+
+sidebarToggle.addEventListener("click", () => {
+  document.body.classList.remove("hamburger-open");
+});
